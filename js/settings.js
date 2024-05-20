aff7e8fe7acb39460af6158ea8ab68c5472cb33a
joamona <joamona@cgf.upv.es>
2024-05-20 16:28:55 +0200

    m

--- js/settings.js
+++ js/settings.js
@@ -1,2 +1,6 @@
-export var URL_API="http://localhost:8000"
-export var URL_GEOSERVER="http://localhost:8080/geoserver"
+//LOCAL
+//export var URL_API="http://localhost:8000"
+//export var URL_GEOSERVER="http://localhost:8080/geoserver"
+
+export var URL_API="https://vps-3206e74d.vps.ovh.net/djdesweb-api"
+export var URL_GEOSERVER="https://vps-3206e74d.vps.ovh.net/geoserver"
